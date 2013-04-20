cb5c79afe9fda28822a9146d649688cd8dca1e19
Filip Procházka <filip@prochazka.su>
2013-04-20 11:36:51 +0200

    implemented blog

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -9,4 +9,17 @@ use Nette;
 
 final class HomePresenter extends Nette\Application\UI\Presenter
 {
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {
+	}
+
+
+	public function renderDefault(): void
+	{
+		$this->template->posts = $this->database
+			->table('posts')
+			->order('created_at DESC')
+			->limit(5);
+	}
 }
