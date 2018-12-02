blaskojan/tutorial-quickstart

added copy of Web Project 2.4 https://github.com/nette/web-project

app/bootstrap.php
@@ -1,0 +1,22 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new Nette\Configurator;
+
+//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+$configurator->enableDebugger(__DIR__ . '/../log');
+
+$configurator->setTimeZone('Europe/Prague');
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__)
+	->register();
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+$container = $configurator->createContainer();
+
+return $container;

nette-examples/quickstart

app/bootstrap.php
@@ -1,0 +1,22 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new Nette\Configurator;
+
+//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+$configurator->enableTracy(__DIR__ . '/../log');
+
+$configurator->setTimeZone('Europe/Prague');
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__)
+	->register();
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+$container = $configurator->createContainer();
+
+return $container;