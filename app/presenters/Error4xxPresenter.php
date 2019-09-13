2ac89c3bd8b85757dfd70cee8ae3ad4cc7c7d246
David Grudl <david@grudl.com>
2019-09-13 12:48:34 +0200

    added copy of Web Project 3.0 https://github.com/nette/web-project

---
+++ app/presenters/Error4xxPresenter.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+
+
+final class Error4xxPresenter extends Nette\Application\UI\Presenter
+{
+	public function startup(): void
+	{
+		parent::startup();
+		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
+			$this->error();
+		}
+	}
+
+
+	public function renderDefault(Nette\Application\BadRequestException $exception): void
+	{
+		// load template 403.latte or 404.latte or ... 4xx.latte
+		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
+		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
+	}
+}
---
+++ app/presenters/ErrorPresenter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\Responses;
+use Nette\Http;
+use Tracy\ILogger;
+
+
+final class ErrorPresenter implements Nette\Application\IPresenter
+{
+	use Nette\SmartObject;
+
+	/** @var ILogger */
+	private $logger;
+
+
+	public function __construct(ILogger $logger)
+	{
+		$this->logger = $logger;
+	}
+
+
+	public function run(Nette\Application\Request $request): Nette\Application\IResponse
+	{
+		$exception = $request->getParameter('exception');
+
+		if ($exception instanceof Nette\Application\BadRequestException) {
+			[$module, , $sep] = Nette\Application\Helpers::splitName($request->getPresenterName());
+			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
+		}
+
+		$this->logger->log($exception, ILogger::EXCEPTION);
+		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
+			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
+				require __DIR__ . '/templates/Error/500.phtml';
+			}
+		});
+	}
+}
